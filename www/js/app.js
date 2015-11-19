a65febdde8a1c09b270994f3d5f8edfc84f59b03
julianesten <julianesten@gmail.com>
2015-11-19 09:01:19 -0500

    cambio de apariencia de la aplicación utilizando material design

--- www/js/app.js
+++ www/js/app.js
@@ -3,247 +3,146 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'ionic-material', 'starter.controllers'])
 
-app.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+        //Registro de las notificaciones con Google Cloud Message
+        PushbotsPlugin.initializeAndroid("5577109c177959db1d8b4567", "483409914041");
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
 
-
-  if(window.Connection) {
-      if(navigator.connection.type == Connection.NONE) {
-        $ionicPopup.confirm({
-          title: 'Network Problem',
-          content: 'Sorry, Please Check Your Network Connection.'
-        })
-        .then(function(result) {
-          if(!result) {
-            navigator.app.exitApp();
-          }
-        });
-      }
-    }
-
-
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    app.receivedEvent('deviceready');
-    
-    PushBotsPlugin.initializeAndroid('5577109c177959db1d8b4567','483409914041');  
-      
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+    });
 })
 
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
 
-
-app.config(function($stateProvider, $urlRouterProvider, $sceDelegateProvider,$ionicConfigProvider) {
- $ionicConfigProvider.tabs.position('bottom');
- $ionicConfigProvider.views.forwardCache(true);
-  //Se asigna el sitio web a embeber desde el iframe
-  $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://maliaccesorios.com/']);
-  $sceDelegateProvider.resourceUrlWhitelist(['self', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.6528930159919!2d-75.58687299999997!3d6.182711000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMTAnNTcuOCJOIDc1wrAzNScxMi43Ilc!5e0!3m2!1ses-419!2sco!4v1436218838254']);
-  $sceDelegateProvider.resourceUrlWhitelist(['self', 'https://v2.zopim.com/widget/livechat.html?key=2lgLHPgIdOriBLzrdpmWzqsGHmf5B0ss&mid=XJf2n0Cl7QI6U8&lang=es-es&hostname=maliaccesorios.com&api_calls=%5B%5B"setStatus"%2C%5Bnull%5D%5D%2C%5B"setStatus"%2C%5Bnull%5D%5D%5D']);
-  $sceDelegateProvider.resourceUrlWhitelist(['self', 'http://maliaccesorios.com/index.php/iniciomobile.html']);
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider 
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/menu.html"
-  })
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      cache: true,
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
+    .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
     })
 
-  .state('tab.collares', {
-      url: '/collares',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/collares.html'
-        }
-      }
-    })
-
-    .state('tab.pulseras', {
-      url: '/pulseras',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/pulseras.html'
-        }
-      }
-    })
-
-    .state('tab.aretes', {
-      url: '/aretes',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/aretes.html'
-        }
-      }
-    })
-
-    .state('tab.complementos', {
-      url: '/complementos',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/complementos.html'
-        }
-      }
-    })
-
-
-  .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  })
-
-  .state('tab.account-formContact',{
-    url: 'formContact',
-          views: {
-        'tab-account': {
-          templateUrl: 'templates/tab-formContact.html'
-        }
-      }
-  })
-
-  .state('tab.location',{
-    url: '/ubicacion',
-          views: {
-        'tab-account': {
-          templateUrl: 'templates/location.html'
-        }
-      }
-  })
-
-    .state('tab.chatOnline',{
-    url: '/chatOnline',
-          views: {
-        'tab-account': {
-          templateUrl: 'templates/chatOnline.html'
-        }
-      }
-  })
-
-  .state('tab.ofertas', {
-    url: '/ofertas',
-    templateUrl: 'templates/ofertas.html'
-
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
-
-
-/**
-
-app.directive('iframeNanny', function($q, $http, $compile, $sce) {
-  return {
-    restrict: 'E',
-    scope: {
-      desiredUri: '=',
-      errorImageUri: '='
-    },
-    link: function(scope, element, attrs) {
-      var loadedUri = '';
-       
-      function isURLReal(fullyQualifiedURL) {
-        var URL = encodeURIComponent(fullyQualifiedURL);
-        var dfd = $q.defer();
-        var yqlUri = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22' + URL + '%22&callback=JSON_CALLBACK';
-        
-        $http.jsonp(yqlUri)
-          .success(function(data, status) {
-            console.log(data.results.length);
-            if (data.results.length) {
-              console.log('success!')
-              dfd.resolve(true);
-            } else {
-              dfd.reject(false);
+    .state('app.contact', {
+        url: '/contact',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/contact.html',
+                controller: 'ListsCtrl'
             }
-          }).error(function(data, status) {
-            dfd.reject('failed');
-          });
-
-        return dfd.promise;
-      }
-
-      scope.$watch('desiredUri', function(uri) {
-        if (loadedUri !== uri) {
-          
-          isURLReal(uri).then(function() { 
-            console.log('directive: uri valid');
-            loadedUri = uri;
-            
-            scope.trustedUri = $sce.trustAsResourceUrl(scope.desiredUri);
-            
-            var iFrameHtml = '<iframe src="{{trustedUri}}" frameborder="0" width="100%" height="1100px" scrolling="auto"></iframe>';
-            
-            var markup = $compile(iFrameHtml)(scope);
-            element.empty();
-            element.append(markup);
-          }).catch(function() {
-            console.log('directive: uri invalid');
-            var badRequestImgHtml = '<img src="{{errorImageUri}}">';
-            
-            var markup = $compile(badRequestImgHtml)(scope);
-            
-            console.log(scope.errorImageUri);
-            element.empty();
-            element.append(markup);
-          });
         }
-      });
-    }
-  };
-});
+    })
 
-*/
+    .state('app.home', {
+        url: '/home',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/home.html',
+                controller: 'MotionCtrl'
+            }
+        }
+    })
+
+    //Estado para el mapa de ubicacion
+    .state('app.location', {
+        url: '/mapLocation',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/mapLocation.html'
+            }
+        }
+    })
+
+    //Estado para el chat en linea
+    .state('app.chatOnline', {
+        url: '/chatOnline',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/chatOnline.html'
+            }
+        }
+    })
+
+    //Estado para escribenos
+    .state('app.escribenos', {
+        url: '/escribenos',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/escribenos.html'
+            }
+        }
+    })
+
+    //Estado para la coleccion nueva
+    .state('app.coleccionNueva', {
+        url: '/coleccionNueva',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/coleccionNueva.html'
+            }
+        }
+    })
+
+    //Estado para collares 
+    .state('app.collares', {
+        url: '/collares',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/collares.html'
+            }
+        }
+    })
+
+    //Estado para pulseras 
+    .state('app.pulseras', {
+        url: '/pulseras',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/pulseras.html'
+            }
+        }
+    })
+
+    //Estado para aretes 
+    .state('app.aretes', {
+        url: '/aretes',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/aretes.html'
+            }
+        }
+    })
+
+    //Estado para complementos 
+    .state('app.complementos', {
+        url: '/complementos',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/complementos.html'
+            }
+        }
+    })
+
+    //Estado para ofertas 
+    .state('app.ofertas', {
+        url: '/ofertas',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/ofertas.html'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/home');
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,66 +1,309 @@
+/* global angular, document, window */
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$ionicPopup, $sce) {
+.controller('AppCtrl', function($scope, $ionicModal, $ionicPopover, $timeout,  $location,  ionicMaterialInk, ionicMaterialMotion) {
+      $timeout(function(){
+    ionicMaterialInk.displayEffect();
+      ionicMaterialMotion.blind();
+  },0);
 
-     // An alert dialog
-  /* $scope.showAlert = function() {
-     var alertPopup = $ionicPopup.alert({
-       title: 'Hola!',
-       template: 'Bienvenido a Mali Accesorios'
-     });
-   };*/
+    // Form data for the login modal
+    $scope.loginData = {};
 
-//document.getElementById('iframeList').contentWindow.document.getElementById('nameDoc').innerHTML
-//Usamos esta bandera para saber si el iframe es cargado o no
-var flag = false;
-   window.uploadDone=function(){
-/*
-var iframe = document.getElementById("awc_frame");
-var iframe_contents = iframe.contentDocument.body.innerHTML;
-alert(iframe_contents);
-*/
-    //=============================
-  //  var contenido = document.getElementById("iframe").contentWindow;
-    //alert(contenido);
-   /* flag = true;
-    alert("flag="+flag);*/
-  // have access to $scope here*
-     /*var alertPopup = $ionicPopup.alert({
-       title: 'Onload!',
-       template: 'Comprueba tu conexión de red'
-     });*/
-   }
-   // sleep(5000);
-     /*setTimeout(function(){
-      alert("timeFunction");
-      if(flag==false){
-        alert("flag="+flag);
-        document.getElementById("iframe").style.display = "none";
+    var navIcons = document.getElementsByClassName('ion-navicon');
+    for (var i = 0; i < navIcons.length; i++) {
+        navIcons[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
 
-      }
+    var fab = document.getElementById('fab');
+    fab.addEventListener('click', function() {
+        return window && window.alert ? window.alert('you clicked the FAB!') : undefined;
+    });
 
-    }, 3000)*/
+    // .fromTemplate() method
+    var template =  '<ion-popover-view>' +
+                    '   <ion-header-bar>' +
+                    '       <h1 class="title">Mali Mobile</h1>' +
+                    '   </ion-header-bar>' +
+                    '   <ion-content class="padding">' +
+                    '       FAQ'+
+                    '   </ion-content>' +
+                    '</ion-popover-view>';
 
-
-
-})
-.controller('embedContactCtrl', function($scope) {})
-
-
-.controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+    $scope.popover = $ionicPopover.fromTemplate(template, {
+        scope: $scope
+    });
+    $scope.closePopover = function() {
+        $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('InkCtrl', function($scope, $stateParams, ionicMaterialInk) {
+    ionicMaterialInk.displayEffect();
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('ComponentsCtrl', function($scope, $stateParams,  ionicMaterialInk, ionicMaterialMotion, $timeout) {
+
+  $timeout(function(){
+    ionicMaterialInk.displayEffect();
+      ionicMaterialMotion.ripple();
+  },0);
+
+    // Toggle Code Wrapper
+    var code = document.getElementsByClassName('code-wrapper');
+    for (var i = 0; i < code.length; i++) {
+        code[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
+})
+
+.controller('ListsCtrl', function($scope, $stateParams,  ionicMaterialMotion) {
+
+    var reset = function() {
+        var inClass = document.querySelectorAll('.in');
+        for (var i = 0; i < inClass.length; i++) {
+            inClass[i].classList.remove('in');
+            inClass[i].removeAttribute('style');
+        }
+        var done = document.querySelectorAll('.done');
+        for (var j = 0; j < done.length; j++) {
+            done[j].classList.remove('done');
+            done[j].removeAttribute('style');
+        }
+        var ionList = document.getElementsByTagName('ion-list');
+        for (var k = 0; k < ionList.length; k++) {
+            var toRemove = ionList[k].className;
+            if (/animate-/.test(toRemove)) {
+                ionList[k].className = ionList[k].className.replace(/(?:^|\s)animate-\S*(?:$|\s)/, '');
+            }
+        }
+    };
+
+    $scope.ripple = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-ripple';
+        setTimeout(function() {
+            ionicMaterialMotion.ripple();
+        }, 500);
+    };
+
+    $scope.fadeSlideInRight = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in-right';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideInRight();
+        }, 500);
+    };
+
+    $scope.fadeSlideIn = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideIn();
+        }, 500);
+    };
+
+    $scope.blinds = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-blinds';
+        setTimeout(function() {
+            ionicMaterialMotion.blinds();
+        }, 500);
+    };
+
+    $scope.blinds();
+
+})
+
+.controller('SetupCtrl', function($scope, $stateParams) {
+    /* ionicMaterialMotion.pushDown({
+        selector: '.push-down'
+    });
+    */
+})
+
+.controller('ExtensionsCtrl', function($scope, $stateParams, $ionicActionSheet, $timeout, $ionicLoading, $ionicModal, $ionicPopup,  ionicMaterialInk) {
+
+    // Triggered on a button click, or some other target
+    $scope.actionSheet = function() {
+
+        // Show the action sheet
+        var hideSheet = $ionicActionSheet.show({
+            buttons: [{
+                text: '<b>Share</b> This'
+            }, {
+                text: 'Move'
+            }],
+            destructiveText: 'Delete',
+            titleText: 'Modify your album',
+            cancelText: 'Cancel',
+            cancel: function() {
+                // add cancel code..
+            },
+            buttonClicked: function(index) {
+                return true;
+            }
+        });
+
+        // For example's sake, hide the sheet after two seconds
+        $timeout(function() {
+            hideSheet();
+        }, 2000);
+
+    };
+
+    $scope.loading = function() {
+        $ionicLoading.show({
+            template: '<div class="loader"><svg class="circular"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div>'
+        });
+
+        // For example's sake, hide the sheet after two seconds
+        $timeout(function() {
+            $ionicLoading.hide();
+        }, 2000);
+    };
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openModal = function() {
+        $scope.modal.show();
+        $timeout(function () {
+            $scope.modal.hide();
+        }, 2000);
+    };
+    // Cleanup the modal when we're done with it
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+
+    // Popover
+    $scope.popover = function() {
+        $scope.$parent.popover.show();
+        $timeout(function () {
+            $scope.$parent.popover.hide();
+        }, 2000);
+    };
+
+    // Confirm
+    $scope.showPopup = function() {
+        var alertPopup = $ionicPopup.alert({
+            title: 'You are now my subscribed to Cat Facts',
+            template: 'You will meow receive fun daily facts about CATS!'
+        });
+
+        $timeout(function() {
+            ionicMaterialInk.displayEffect();
+        }, 0);
+    };
+
+    // Toggle Code Wrapper
+    var code = document.getElementsByClassName('code-wrapper');
+    for (var i = 0; i < code.length; i++) {
+        code[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+        });
+    }
+})
+
+.controller('MotionCtrl', function($scope, $stateParams, $timeout,  ionicMaterialMotion, ionicMaterialInk) {
+    var fab = document.getElementById('fab');
+
+    $scope.moveFab = function(dir) {
+        fab.style.display = 'none';
+        fab.className = fab.className.replace('button-fab-top-left', '');
+        fab.className = fab.className.replace('button-fab-top-right', '');
+        fab.className = fab.className.replace('button-fab-bottom-left', '');
+        fab.className = fab.className.replace('button-fab-bottom-right', '');
+        fab.className += ' button-fab-' + dir;
+        $timeout(function() {
+            fab.style.display = 'block';
+        }, 100);
+    };
+
+    $scope.motionFab = function(type) {
+        var shouldAnimate = false;
+        var classes = type instanceof Array ? type : [type];
+
+        function toggleMotionClass (theClass) {
+            $timeout(function() {
+                fab.classList.toggle(theClass);
+            }, 300);
+        }
+
+        for (var i = 0; i < classes.length; i++) {
+            fab.classList.toggle(classes[i]);
+
+            shouldAnimate = fab.classList.contains(classes[i]);
+            if (shouldAnimate) {
+                (toggleMotionClass)(classes[i]);
+            }
+        }
+    };
+
+    var reset = function() {
+        var inClass = document.querySelectorAll('.in');
+        for (var i = 0; i < inClass.length; i++) {
+            inClass[i].classList.remove('in');
+            inClass[i].removeAttribute('style');
+        }
+        var done = document.querySelectorAll('.done');
+        for (var j = 0; j < done.length; j++) {
+            done[j].classList.remove('done');
+            done[j].removeAttribute('style');
+        }
+        var ionList = document.getElementsByTagName('ion-list');
+        for (var k = 0; k < ionList.length; k++) {
+            var toRemove = ionList[k].className;
+            if (/animate-/.test(toRemove)) {
+                ionList[k].className = ionList[k].className.replace(/(?:^|\s)animate-\S*(?:$|\s)/, '');
+            }
+        }
+    };
+
+    $scope.ripple = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-ripple';
+        setTimeout(function() {
+            ionicMaterialMotion.ripple();
+        }, 500);
+    };
+
+    $scope.fadeSlideInRight = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in-right';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideInRight();
+        }, 500);
+    };
+
+    $scope.fadeSlideIn = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-fade-slide-in';
+        setTimeout(function() {
+            ionicMaterialMotion.fadeSlideIn();
+        }, 500);
+    };
+
+    $scope.blinds = function() {
+        reset();
+        document.getElementsByTagName('ion-list')[0].className += ' animate-blinds';
+        setTimeout(function() {
+            ionicMaterialMotion.blinds();
+        }, 500);
+    };
+
+    $scope.blinds();
+    ionicMaterialInk.displayEffect();
 });
-
